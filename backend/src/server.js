0d216d5645b435e6cf42991d87ce8bc2545f107c
SamuelASC <40130164+SamuelASC@users.noreply.github.com>
2023-08-19 16:41:59 -0300

    Adicao do frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,13 @@
 const express = require('express'); //importação do express
+const routes = require('./routes');
 const app = express(); //criação do objeto chamado app com o express
-
+const cors = require('cors');
 app.use(express.json()); //Usando o json
-
+app.use(routes);
+app.use(cors());
 //Criação da primeira rota de teste
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Hello World');
 });
 
 //Subir em uma porta especifica, porta 3000 e a porta padrao do Express
